c67ce93bfa6d0f27499a6037e37899ac43d4ad99
cardaminexhz <zhxh69776@qq.com>
2016-06-05 21:55:35 +0800

    task16 another solution; 	new file:   baiduProj/task16/improve/js-task04-improve.html 	new file:   baiduProj/task16/improve/js-task04-improve.js

--- baiduProj/task16/improve/js-task04-improve.js
+++ baiduProj/task16/improve/js-task04-improve.js
@@ -1,5 +1,5 @@
 /**
- * @comment 点击按钮时验证输入逻辑；提示方式：自定义文本。
+ * @comment 输入框失去焦点时验证输入逻辑；提示方式：自定义文本。
  *          再一次使用了 arrayObj.forEach 和 arrayObj.filter()，代码逻辑清晰。
  * Created by cardaminexhz on 2016/6/1.
  */
@@ -19,39 +19,54 @@ var $ = function(id) {
 };
 
 /**
+ * 输入框失去焦点时，验证有效性
+ * @param type
+ *          验证类型。1.城市名：中英文字符；2.空气质量：整数
+ * @param id
+ *          触发事件的节点id
+ */
+function checkInput(type, id) {
+    // regexp：中英文字符
+    var characterPattern = new RegExp("^([\u4E00-\u9FA5]|[A-Za-z])+$");
+    // regexp：整数
+    var digitalPattern = new RegExp("^[0-9]+$"); // /^\d+$/
+
+    var str = $(id).value.trim();
+    var validFlag = true;
+
+    // 1 中英文字符
+    if(type === 1) {
+        if(!characterPattern.test(str)) {
+            validFlag = false;
+        }
+    }
+    // 2 整数
+    if(type === 2) {
+        if(!digitalPattern.test(str)) {
+            validFlag = false;
+        }
+    }
+
+    if(validFlag) {
+        $(id+"-errorhint").style.visibility = "hidden";
+        $('add-btn').removeAttribute("disabled");
+    } else {
+        // 表单验证无效：显示错误提示信息；禁用提交按钮。
+        $(id+"-errorhint").style.visibility = "visible";
+        $('add-btn').disabled = "disabled";
+    }
+}
+
+
+/**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-    var city = $("aqi-city-input").value.trim();
-    var value = $("aqi-value-input").value.trim();
-    var cityPattern = new RegExp("^([\u4E00-\u9FA5]|[A-Za-z])+$");
-    var valuePattern = new RegExp("^[0-9]+$"); // /^\d+$/
-    var formValid = true;
-
-    if(!cityPattern.test(city)) {
-        $("city-error-info").style.visibility = "visible";
-        console.log('city invalid: ' + city);
-        formValid = false;
-    }
-    if(!valuePattern.test(value)) {
-        $("value-error-info").style.visibility = "visible";
-        console.log('value invalid: ' + value);
-        formValid = false;
-    }
-
-    if(!formValid) {
-        // 表单未通过验证，按钮无效
-        $('add-btn').disabled = "disabled";
-    } else {
-        // 表单验证通过，将用户输入添加到aqiData
-        var item = {};
-        item.name = city;
-        item.value = value;
-        aqiData.push(item);
-    }
-
-    console.log("aqiData: " + aqiData);
+    var item = {};
+    item.name = $("aqi-city-input").value.trim();
+    item.value = $("aqi-value-input").value.trim();
+    aqiData.push(item);
 }
 
 /**
@@ -93,8 +108,8 @@ function delBtnHandle(deleteIndex) {
 function reset() {
     $("aqi-city-input").value = '';
     $("aqi-value-input").value = '';
-    $("city-error-info").style.visibility = "hidden";
-    $("value-error-info").style.visibility = "hidden";
+    $("aqi-city-input-errorhint").style.visibility = "hidden";
+    $("aqi-value-input-errorhint").style.visibility = "hidden";
     $('add-btn').removeAttribute("disabled");
 }
 
