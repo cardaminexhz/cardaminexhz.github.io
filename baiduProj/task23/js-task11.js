69bf1b53176913dd0de3bfc707f13097de67f6a5
cardaminexhz <zhxh69776@qq.com>
2016-07-20 10:30:48 +0800

    task 23 in progress;

--- baiduProj/task23/js-task11.js
+++ baiduProj/task23/js-task11.js
@@ -4,24 +4,63 @@ var $ = function(id) { return document.getElementById(id); },
     timer;
 
 $("DFS-btn").onclick = function() {
+    init();
     dfTraverse(rootNode);
-    console.log(nodeList);
+    render();
+}
+$("BFS-btn").onclick = function() {
+    init();
+    bfTraverse(rootNode);
+    render();
 }
 
-// 深度优先遍历 depth - first
+// 深度优先遍历 depth - first  栈-递归
 function dfTraverse (node) {
-    if((nodeList.length != 0) && (node === rootNode)) {
-        return;
+    if(node) {
+        nodeList.push(node);
+        for(var i = 0; i < node.children.length; i++) {
+            dfTraverse(node.children[i]);
+        }
     }
-    // 找到最左子节点
-    while( (node != null) && (node.childElementCount != 0) ) {
-        dfTraverse(node.firstElementChild);
+}
+
+// 广度优先遍历 breadth - first  队列
+function bfTraverse (rootNode) {
+    var queue = [],
+        visitNode;
+
+    queue.push(rootNode);
+    while(queue.length) {
+        visitNode = queue.shift();
+        nodeList.push(visitNode);
+
+        for(var i = 0; i < visitNode.children.length; i++) {
+            queue.push(visitNode.children[i]);
+        }
     }
-    nodeList.push(node);
-    // 同胞节点
-    while(node.nextElementSibling != null) {
-        dfTraverse(node.nextElementSibling);
-    }
-    // 父节点
-    dfTraverse(node.parentElement);
+}
+
+
+// 初始化
+function init() {
+    nodeList.forEach(function(item){
+        item.style.backgroundColor = '#fff';
+    });
+    nodeList = [];
+}
+
+// 颜色渲染
+function render() {
+    var i = 0;
+    nodeList[i].style.backgroundColor = 'blue';
+    timer = setInterval(function(){
+        i++;
+        if(i < nodeList.length) {
+            nodeList[i-1].style.backgroundColor = '#fff';
+            nodeList[i].style.backgroundColor = 'blue';
+        } else {
+            clearInterval(timer);
+            nodeList[nodeList.length-1].style.backgroundColor = '#fff';
+        }
+    }, 1000)
 }
